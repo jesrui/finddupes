08f58f2a34f4c63ff551a43660c61d33a62bddde
Jesus Ruiz <jesrui@gmail.com>
2014-06-17 12:27:40 +0200

    add licence and pump version

--- finddupes.c
+++ finddupes.c
@@ -1,4 +1,27 @@
 // vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+/*
+    finddupes -- finds duplicate files in a given set of directories
+
+    Copyright (C) 2014 Jesús Ruiz de Infante
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    DEALINGS IN THE SOFTWARE.
+*/
 
 #include <assert.h>
 #include <ctype.h>
@@ -34,7 +57,7 @@ KLIST_INIT(str, const char *, __nop_free)
 KLIST_INIT(inodev, struct inodev, __nop_free)
 KHASH_MAP_INIT_STR(str, klist_t(str)*)
 
-const char VERSION[] = "0.1";
+const char VERSION[] = "0.2";
 int flags;
 char *sep = "\n";
 size_t seplen = 1;
